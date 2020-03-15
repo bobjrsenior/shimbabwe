b47802821d20394ad1d31ab6c80b477985aec79f
bobjrsenior <bobjrsenior@bobjrsenior.pw>
2020-03-15 00:39:25 -0400

    Make title and end scene. Add transitions between them

---
+++ Shimbabwe/Assets/Scripts/GenericUIButtons.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GenericUIButtons : MonoBehaviour
+{
+    private string mainMenuSceneName = "Title_Screen";
+    private string moreInfoSceneName = "Path_Description";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void BeginFromTitleScreen()
+    {
+        // +2 to avoid the more info scene
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+    }
+
+    public void MoreInfoScene()
+    {
+        SceneManager.LoadScene(moreInfoSceneName);
+    }
+
+    public void ReturnToChooseFromMission()
+    {
+        SceneManager.LoadScene(mainMenuSceneName);
+    }
+
+    public void exitGame()
+    {
+        Application.Quit();
+    }
+}
